4f4b1063450ab3631ff714af66ab110e59016736
ohmatt <jmatt306@gmail.com>
2013-04-19 17:55:58 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taskificate::Application.config.secret_token = '182ee7e1a306881eda3f3871ca8969c1d51c8c550999dc32ad08435eb70b23d6d16a09b3e4865395fc7abaa30cc8e76af4955dfc2bfce1a8d3af2507487a1d9f'
